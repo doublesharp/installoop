9fc604ffc84d0a6e88819d8e404de1aa09c08e24
Bartholomé Gili <bartho.gili@gmail.com>
2022-10-13 23:23:30 +0200

    fix: error if path does not exists

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,12 @@
+import { existsSync } from "fs"
 import { readdir } from "fs/promises"
 import { resolve } from "path"
 
 export async function getPackages(dirPath: string, includeModules: boolean, skipRoot: boolean = false): Promise<string[]> {
     
     let output: string[] = []
+
+    if (!existsSync(dirPath)) return []
     
     const paths = await readdir(dirPath, { withFileTypes: true })
     
